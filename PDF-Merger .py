00473c47023d61ba80241d731c0fbcaf921fab3d
M.Haseeb <haseebashfaq700@gmail.com>
2026-07-02 15:59:48 +0500

    Update PDF-Merger .py

--- PDF-Merger .py
+++ PDF-Merger .py
@@ -2,7 +2,7 @@ from pypdf import PdfWriter
 
 merger = PdfWriter()
 files = [
-    "Enter your PDF's here you want to merge"
+    # "Enter your PDF's here you want to merge"
 ]
 for file in files:
     merger.append(file)
